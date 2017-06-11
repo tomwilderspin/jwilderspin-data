76745952a311c5c46fc95f3197b2d3250154654b
tomwilderspin <tomwilderspin@gmail.com>
2017-06-11 22:56:03 +0100

    updated handler

--- src/contact/handler.js
+++ src/contact/handler.js
@@ -2,17 +2,41 @@
 
 const AWS = require('aws-sdk');
 
+const sns = new AWS.SNS();
+
 module.exports.index = (event, context, callback) => {
 
-  console.log(JSON.stringify({input: event}));
+  const inputData = event.body;
 
-  //canned success response
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'success',
-    }),
+  console.log(inputData);
+
+  const params = {
+    Message: JSON.stringify(inputData),
+    Subject: 'New Contact Form Submission',
+    TopicArn: "arn:aws:sns:eu-west-1:010704906608:jwilderspin-contact"
   };
 
-  callback(null, response);
+  return new Promise((resolve, reject) => {
+    sns.publish(params, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+
+  })
+  .then((data) => {
+    console.log(data);
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({message: "success"}),
+    });
+  })
+  .catch((err) => {
+    console.log(err, err.stack);
+    callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({message: "fail"}),
+    });
+  });
 };
